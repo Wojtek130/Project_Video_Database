dcbf5aa9aefb02a03b5d8fc267cc931a5e8a476f
Wojtek <pimpek86@gmail.com>
2021-06-22 12:03:46 +0200

    test db created

---
+++ tests/file.py
@@ -0,0 +1,2 @@
+class Ojoj:
+    pass
--- tests/test_db.py
+++ tests/test_db.py
@@ -1,5 +1,10 @@
 import unittest
 import sqlite3
+#import sys
+#sys.path.insert(1, '/.../application/app/folder')
+#sys.path.append("..")
+import os
+from . import *
 
 
 class TestDB(unittest.TestCase):
@@ -15,9 +20,10 @@ class TestDB(unittest.TestCase):
     def setUp(self):
         self.conn = sqlite3.connect("database/test.db")
         c = self.conn.cursor()
-        cmd = "CREATE TABLE video (trend TEXT, trend_id1 TEXT, trend_id2 TEXT, trend_id3 TEXT, datetime TEXT)"
-        c.execute(cmd)
-        self.conn.commit()
+       # vid_1 = Video()
+      #  cmd = "CREATE TABLE video (trend TEXT, trend_id1 TEXT, trend_id2 TEXT, trend_id3 TEXT, datetime TEXT)"
+      #  c.execute(cmd)
+     #   self.conn.commit()
 
     def tearDown(self):
          self.conn.close()
@@ -26,4 +32,6 @@ class TestDB(unittest.TestCase):
         self.assertEqual(1 + 2, 3)
 
 if __name__ == "__main__":
+    #print(os.getcwd())
     unittest.main()
+    
